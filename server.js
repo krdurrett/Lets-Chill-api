2bd4c6358ae95d8ae1200c3e7b1f9964f2e36976
Kayla Durrett <kayla.durrett@gmail.com>
2022-02-28 12:21:40 -0700

    Fix path in get requests and confirm they are working

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const feelings = require('./data/feelings.js')
 const actions = require('./data/actions.js')
+require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
@@ -18,40 +18,41 @@ app.get('/', (request, response) => {
   response.send('Get ready to chill out!')
 })
 
-app.get('api/v1/feelings', (request, response) => {
+app.get('/api/v1/feelings', (request, response) => {
   const feelings = app.locals.feelings
-
-  response.status(200).json({ feelings })
+  response.status(200).json(feelings)
 })
 
-app.get('api/v1/feelings/:id', (request, response) => {
+app.get('/api/v1/feelings/:id', (request, response) => {
   const { id } = request.params
-  const feeling = app.locals.feelings.find(feeling => feeling.id === parseInt(id))
+  const { feelings } = app.locals
+  const feeling = feelings.find(feeling => feeling.id === parseInt(id))
   if (!feeling) {
     return response.status(404).json({
       message: `No feeling found with the id of #${id}.`
     })
   }
 
-  response.status(200).json({ feeling })
+  response.status(200).json(feeling)
 })
 
-app.get('api/v1/actions', (request, response) => {
+app.get('/api/v1/actions', (request, response) => {
   const actions = app.locals.actions
 
-  response.status(200).json({ actions })
+  response.status(200).json(actions)
 })
 
-app.get('api/v1/actions/:id', (request, response) => {
+app.get('/api/v1/actions/:id', (request, response) => {
   const { id } = request.params
-  const action = app.locals.actions.find(action => action.id === parseInt(id))
+  const { actions } = app.locals
+  const action = actions.find(action => action.id === parseInt(id))
   if (!action) {
     return response.status(404).json({
       message: `No action found with the id of #${id}.`
     })
   }
 
-  response.status(200).json({ action })
+  response.status(200).json(action)
 })
 
 app.listen(app.get('port'), () => {
