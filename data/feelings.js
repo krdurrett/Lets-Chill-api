d8abd51e0cc312abc4d3e4f8c40a14749e5c9b41
Kayla Durrett <kayla.durrett@gmail.com>
2022-02-28 08:15:47 -0700

    Add ids to actions in feelingstable

--- data/feelings.js
+++ data/feelings.js
@@ -1,7 +1,63 @@
 module.exports = [
-  { id: 1, feeling: 'I\'m gonna die!', actions: ['Call 911', 'Stay Where You Are', 'Breathe', 'State the Situation', 'Shift Focus', '3-3-3 Rule', 'Take Meds']},
-  { id: 2, feeling: 'ALERT!', actions: ['Breathe', 'State The Situation', 'Shift Focus', 'Close your Eyes', 'Find a Happy Place', 'Hold an Ice Cube', 'Take Meds']},
-  { id: 3, feeling: 'Uncomfortable!', actions: ['Change the Scenery', 'Repeat a Mantra', 'Breathe', 'Call a Friend', 'Aromatherapy', 'Tapping', 'Progressive Muscle Relaxation']},
-  { id: 4, feeling: 'Something\'s Off...', actions: ['Light Exercise', 'Hot Shower', 'Stay Away from Sugar', 'Write Down your Thoughts', 'Watch a Funny Video', 'Stay Present']},
-  { id: 5, feeling: 'Calm', actions: ['Exercise', 'Avoid Alcohol and Drugs', 'Cut back on caffeine', 'Prioritize Sleep', 'Eat Healthy', 'Stick to Treatment Plan', 'Identify Triggers', 'Journal', 'Socialize']}
+  { id: 1, 
+    feeling: 'I\'m gonna die!', 
+    actions: [
+      {id: 1, action: 'Call 911'}, 
+      {id: 2, action: 'Stay Where You Are'}, 
+      {id: 3, action: 'Breathe'}, 
+      {id: 4, action: 'State the Situation'}, 
+      {id: 5, action: 'Shift Focus'},
+      {id: 6, action: '3-3-3 Rule'}, 
+      {id: 7, action: 'Take Meds'}
+    ]
+  },
+  { id: 2, 
+    feeling: 'ALERT!', 
+    actions: [
+      {id: 3, action: 'Breathe'}, 
+      {id: 4, action: 'State the Situation'}, 
+      {id: 5, action: 'Shift Focus'}, 
+      {id: 8, action: 'Close your Eyes'}, 
+      {id: 9, action: 'Find a Happy Place'}, 
+      {id: 10, action: 'Hold an Ice Cube'}, 
+      {id: 7, action: 'Take Meds'}
+    ]
+  },
+  { id: 3, 
+    feeling: 'Uncomfortable!', 
+    actions: [
+      {id: 11, action: 'Change the Scenery'}, 
+      {id: 12, action: 'Repeat a Mantra'}, 
+      {id: 3, action: 'Breathe'}, 
+      {id: 13, action: 'Call a Friend'}, 
+      {id: 14, action: 'Aromatherapy'}, 
+      {id: 15, action: 'Tapping'}, 
+      {id: 31, action: 'Progressive Muscle Relaxation'}
+    ]
+  },
+  { id: 4, 
+    feeling: 'Something\'s Off...', 
+    actions: [
+      {id: 16, action: 'Light Exercise'}, 
+      {id: 17, action: 'Hot Shower'}, 
+      {id: 18, action: 'Stay Away from Sugar'}, 
+      {id: 19, action: 'Write Down your Thoughts'}, 
+      {id: 20, action: 'Watch a Funny Video'}, 
+      {id: 21, action: 'Stay Present'}
+    ]
+  },
+  { id: 5, 
+    feeling: 'Calm', 
+    actions: [
+      {id: 22, action: 'Exercise'}, 
+      {id: 23, action: 'Avoid Alcohol and Drugs'}, 
+      {id: 24, action: 'Cut back on caffeine'}, 
+      {id: 25, action: 'Prioritize Sleep'}, 
+      {id: 26, action: 'Eat Healthy'}, 
+      {id: 27, action: 'Stick to Treatment Plan'}, 
+      {id: 28, action: 'Identify Triggers'}, 
+      {id: 29, action: 'Journal'}, 
+      {id: 30, action: 'Socialize'}
+    ]
+  }
 ]
