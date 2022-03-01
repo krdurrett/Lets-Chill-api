5e2e3552ccdb1e2ea9d178402141e70b9c34a08e
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-01 09:58:27 -0700

    Add a log tale to track users feelings and actions

--- data/feelings.js
+++ data/feelings.js
@@ -5,7 +5,7 @@ module.exports = [
       {id: 1, action: 'Call 911'}, 
       {id: 2, action: 'Stay Where You Are'}, 
       {id: 3, action: 'Breathe'}, 
-      {id: 4, action: 'State the Situation'}, 
+      {id: 4, action: 'State The Situation'}, 
       {id: 5, action: 'Shift Focus'},
       {id: 6, action: '3-3-3 Rule'}, 
       {id: 7, action: 'Take Meds'}
@@ -15,21 +15,21 @@ module.exports = [
     feeling: 'ALERT!', 
     actions: [
       {id: 3, action: 'Breathe'}, 
-      {id: 4, action: 'State the Situation'}, 
+      {id: 4, action: 'State The Situation'}, 
       {id: 5, action: 'Shift Focus'}, 
-      {id: 8, action: 'Close your Eyes'}, 
-      {id: 9, action: 'Find a Happy Place'}, 
-      {id: 10, action: 'Hold an Ice Cube'}, 
+      {id: 8, action: 'Close Your Eyes'}, 
+      {id: 9, action: 'Find A Happy Place'}, 
+      {id: 10, action: 'Hold An Ice Cube'}, 
       {id: 7, action: 'Take Meds'}
     ]
   },
   { id: 3, 
     feeling: 'Uncomfortable!', 
     actions: [
-      {id: 11, action: 'Change the Scenery'}, 
-      {id: 12, action: 'Repeat a Mantra'}, 
+      {id: 11, action: 'Change The Scenery'}, 
+      {id: 12, action: 'Repeat A Mantra'}, 
       {id: 3, action: 'Breathe'}, 
-      {id: 13, action: 'Call a Friend'}, 
+      {id: 13, action: 'Call A Friend'}, 
       {id: 14, action: 'Aromatherapy'}, 
       {id: 15, action: 'Tapping'}, 
       {id: 31, action: 'Progressive Muscle Relaxation'}
@@ -40,9 +40,9 @@ module.exports = [
     actions: [
       {id: 16, action: 'Light Exercise'}, 
       {id: 17, action: 'Hot Shower'}, 
-      {id: 18, action: 'Stay Away from Sugar'}, 
-      {id: 19, action: 'Write Down your Thoughts'}, 
-      {id: 20, action: 'Watch a Funny Video'}, 
+      {id: 18, action: 'Stay Away From Sugar'}, 
+      {id: 19, action: 'Write Down Your Thoughts'}, 
+      {id: 20, action: 'Watch A Funny Video'}, 
       {id: 21, action: 'Stay Present'}
     ]
   },
@@ -50,11 +50,11 @@ module.exports = [
     feeling: 'Calm', 
     actions: [
       {id: 22, action: 'Exercise'}, 
-      {id: 23, action: 'Avoid Alcohol and Drugs'}, 
-      {id: 24, action: 'Cut back on caffeine'}, 
+      {id: 23, action: 'Avoid Alcohol And Drugs'}, 
+      {id: 24, action: 'Cut Back On Caffeine'}, 
       {id: 25, action: 'Prioritize Sleep'}, 
       {id: 26, action: 'Eat Healthy'}, 
-      {id: 27, action: 'Stick to Treatment Plan'}, 
+      {id: 27, action: 'Stick To Treatment Plan'}, 
       {id: 28, action: 'Identify Triggers'}, 
       {id: 29, action: 'Journal'}, 
       {id: 30, action: 'Socialize'}
