e633eb5dc38f95aad210e26d9636823a61982e4e
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-04 16:36:29 -0700

    Change sample dates to correct format

--- data/log.js
+++ data/log.js
@@ -1,5 +1,5 @@
 module.exports = [
-  {id: 1, date: '02/28/2022', feeling: 'Uncomfortable', action: 'Change the Scenery', helped: true},
-  {id: 2, date: '01/30/2022', feeling: 'ALERT!', action: 'Close your Eyes', helped: true},
-  {id: 3, date: '01/15/2022', feeling: 'I\'m gonna die!', action: 'Stay Where You Are', helped: false},
+  {id: 1, date: '2/28/2022', feeling: 'Uncomfortable', action: 'Change the Scenery', helped: true},
+  {id: 2, date: '1/30/2022', feeling: 'ALERT!', action: 'Close your Eyes', helped: true},
+  {id: 3, date: '1/15/2022', feeling: 'I\'m gonna die!', action: 'Stay Where You Are', helped: false},
 ]
