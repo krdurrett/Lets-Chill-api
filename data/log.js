7726b589eadefb2c248e74f2b24ca492974e9d23
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-04 16:41:50 -0700

    Get rid of fakelog data

--- data/log.js
+++ data/log.js
@@ -1,5 +1,3 @@
 module.exports = [
-  {id: 1, date: '2/28/2022', feeling: 'Uncomfortable', action: 'Change the Scenery', helped: true},
-  {id: 2, date: '1/30/2022', feeling: 'ALERT!', action: 'Close your Eyes', helped: true},
-  {id: 3, date: '1/15/2022', feeling: 'I\'m gonna die!', action: 'Stay Where You Are', helped: false},
+
 ]
